6cbd0cacafa180e1ed106dae12bfec870e1ece75
alex-merren <alex.merren@hpe.com>
2022-03-22 17:12:11 +0000

    *

--- solver.py
+++ solver.py
@@ -2,7 +2,8 @@ import os
 import re
 import flag
 
-ALL_WORDS_FILE = os.path.abspath("words.txt")
+ALL_WORDS_FILE = os.path.abspath("all_words.txt")
+FIVE_LETTER_WORDS_FILE = os.path.abspath("five_letter_words.txt")
 WORD_SIZE = 6  
 
 def define_flags():
@@ -11,10 +12,16 @@ def define_flags():
     green = flag.string("green", "", "The letters that are highlighted in green, along with their position: 'A' in position 1 would be \"a1\".")
     return grey, yellow, green
 
-def load_words(filename: str):
+def load_all_words_for_length(filename):
     words = []
     with open(filename) as words_file:
         [words.append(line.rstrip()) for line in words_file if len(line) == WORD_SIZE]
+    return words
+
+def load_words(filename):
+    words = []
+    with open(filename) as words_file:
+        [words.append(line.rstrip()) for line in words_file]
     return words
 
 def build_expression_must_contain_one(grey_string):
@@ -68,7 +75,7 @@ def filter_words(words, grey_string, yellow_string, green_string):
 def main():
     grey, yellow, green = define_flags()
     flag.parse()
-    words = load_words(ALL_WORDS_FILE)
+    words = load_words(FIVE_LETTER_WORDS_FILE)
     final_words = filter_words(words, grey, yellow, green)
     print(final_words)
 
